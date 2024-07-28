9dc1b62609a24a219abe01c83cd9de7fe04eeef8
julsCadenas <jscadenas12@gmail.com>
2024-07-28 21:11:30 +0800

    fixed trailer component

--- src/components/trailers.jsx
+++ src/components/trailers.jsx
@@ -12,27 +12,33 @@ const Trailers = ({ animeLatest }) => {
 
     return (
         <section className="mt-14">
-            <div className="flex flex-col font-Montserrat bg-black w-full max-w-3xl">
+            <div className="flex flex-col font-Montserrat bg-black">
                 {randomAnime ? (
                     <>
-                        {/* <p className="text-neutral-50 text-lg font-medium mb-2 text-center">{randomAnime.title}</p> */}
-                        <p className="text-neutral-50 text-2xl font-medium mb-2">Airing Now</p>
-                        <div className="flex flex-col sm:flex-row w-full">
-                            {randomAnime.trailer && randomAnime.trailer.embed_url ? (
-                                <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
+                        <div className="flex flex-col md:flex-row">
+                            <aside className="flex flex-col text-neutral-50 max-w-4xl w-full mr-5">
+                                <p className="text-neutral-50 text-2xl font-bold mb-3">Airing Now</p>
+                                <p className="text-3xl md:text-5xl font-bold mb-2">{randomAnime.title}</p>
+                                <p className="text-lg md:text-xl mb-2"><strong>Episodes: </strong> {randomAnime.episodes}</p>
+                                <p className="text-lg md:text-xl mb-2"><strong>Status: </strong> {randomAnime.status}</p>
+                                <p className="text-lg md:text-xl mb-2"><strong>Airing:</strong> {randomAnime.aired.string}</p>
+                                <p className="text-xs md:text-sm mb-2 text-justify">{randomAnime.synopsis}</p>
+                                <button className="bg-neutral-50 text-black text-lg rounded-md w-24 font-bold hover:bg-gray-300 mt-2">See All</button>
+                            </aside>
+                            <div className="relative w-full h-auto md:pb-[40%] pb-[56.25%] overflow-hidden rounded-md mt-4 mb-4 md:mt-0 md:mb-0">
+                                {randomAnime.trailer && randomAnime.trailer.embed_url ? (
                                     <iframe
-                                        src={randomAnime.trailer.embed_url}
+                                        src={`${randomAnime.trailer.embed_url}?autoplay=0`}
                                         frameBorder="0"
                                         allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                         allowFullScreen
                                         title={randomAnime.title}
-                                        className="absolute top-0 left-0 w-full h-full"
-                                    ></iframe>
-                                    <p className='text-neutral-50'>{randomAnime.title}</p>
-                                </div>
-                            ) : (
-                                <p className='text-neutral-50'>No trailer available.</p>
-                            )}
+                                        className="absolute top-0 left-0 w-full h-full md:h-4/5"
+                                    />
+                                ) : (
+                                    <p className='text-neutral-50'>No trailer available.</p>
+                                )}
+                            </div>
                         </div>
                     </>
                 ) : (
