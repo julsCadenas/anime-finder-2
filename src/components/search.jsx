d6061481d0792bf10813c52765a60c67611adb4d
julsCadenas <jscadenas12@gmail.com>
2024-07-30 22:53:31 +0800

    finished searchbar (static)

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,24 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import fetchAnime from '../modules/fetchanime';
+import Loading from './loading';
 
 const Search = () => {
-    return(
+    const [animeList, setAnimeList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [title, setTitle] = useState('');
+
+    useEffect(() => {
+        if (title.trim() === '') {
+            setIsLoading(false);
+            setAnimeList([]);
+            return;
+        }
+
+        const link = `https://api.jikan.moe/v4/anime?q=${title}`;
+
+        setIsLoading(true);
+        fetchAnime(link, (data) => {
+            setAnimeList(data);
+            setIsLoading(false);
+        });
+    }, [title]);
+
+    const handleInputChange = (e) => {
+        setTitle(e.target.value);
+    };
+
+    return (
         <div className="bg-black bg-opacity-5 backdrop-filter backdrop-blur-sm h-screen w-screen transition-all">
-            <form className="max-w-md mx-auto">   
+            <form className="max-w-md mx-auto" onSubmit={(e) => e.preventDefault()}>
                 <div className="relative">
-                    <input 
+                    <input
                         type="text"
                         id="default-search"
+                        value={title}
+                        onChange={handleInputChange}
                         className="max-w-96 max-h-20 w-80 h-full md:w-full md:h-full pl-5 md:pl-8 bg-black bg-opacity-5 backdrop-filter backdrop-blur-sm border-b-2 border-transparent 
                                     focus:border-neutral-50 focus:outline-none font-Montserrat text-lg md:text-xl 
-                                    text-neutral-50 mt-20 transition-colors duration-300" 
+                                    text-neutral-50 mt-12 md:mt-20 transition-colors duration-300"
                         placeholder="Search for Anime here"
                     />
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/></svg>
-                    </button>
                 </div>
             </form>
+            {isLoading ? (
+                <Loading />  
+            ) : (
+                <div className="flex justify-center overflow-y-auto h-[calc(100vh-96px)]">
+                    <ul className="text-neutral-50 flex flex-col items-start font-Montserrat w-full max-w-3xl">
+                        {animeList.map((anime) => (
+                            <li key={anime.mal_id} className='text-xl flex items-center gap-3 mt-5'>
+                                <img src={anime.images.jpg.large_image_url}
+                                    className='w-16 h-auto rounded-md' />
+                                {anime.title}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
-    )
+    );
 };
 
 export default Search;
