8f531727161e37675129fecc8767b4bdbd70371b
julsCadenas <jscadenas12@gmail.com>
2024-08-03 13:09:54 +0800

    added svg icons on read more read less

--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -62,13 +62,23 @@ const Details = () => {
                                 <p><strong>Demographics: </strong>{anime.demographics && anime.demographics.map(demo => demo.name).join(', ')}</p>
                                 <p><strong>Rating: </strong>{anime.rating}</p>
                                 <p className="mt-1"><strong>Synopsis:</strong></p>
-                                <div className="text-justify inline">
-                                    {isExpanded ? anime.synopsis : `${anime.synopsis.slice(0, 200)}...`}
+                                <div className="text-justify">
+                                    <p className="inline">{isExpanded ? anime.synopsis : `${anime.synopsis.slice(0, 200)}...`}</p>
                                     <button
-                                        className="text-gray-300 hover:text-gray-500 focus:outline-none ml-2"
+                                        className="text-gray-300 hover:text-gray-500 focus:outline-none ml-2 flex items-center"
                                         onClick={toggleExpanded}
                                     >
-                                        <strong>{isExpanded ? 'Read Less' : 'Read More'}</strong>
+                                        {isExpanded ? (
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                                <path d="M5 15l7-7 7 7" />
+                                            </svg>
+
+                                        ) : (
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                                <path d="M19 9l-7 7-7-7" />
+                                            </svg>
+                                        )}
+                                        <strong>{isExpanded ? ' Read Less' : ' Read More'}</strong>
                                     </button>
                                 </div>
                             </section>
