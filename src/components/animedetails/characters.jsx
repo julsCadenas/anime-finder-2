f3bbf85d15216d3fb677df875760fed0ec339b6a
julsCadenas <jscadenas12@gmail.com>
2024-08-05 23:57:02 +0800

    added en jp toggle to characters

--- src/components/animedetails/characters.jsx
+++ src/components/animedetails/characters.jsx
@@ -5,6 +5,7 @@ import Loading from "../loading";
 const Characters = ({ id }) => {
     const [anime, setAnime] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [showEnglish, setShowEnglish] = useState(false); 
     const link = `https://api.jikan.moe/v4/anime/${id}/characters`;
 
     useEffect(() => {
@@ -14,38 +15,59 @@ const Characters = ({ id }) => {
         });
     }, [id]);
 
+    const handleToggleChange = (event) => {
+        setShowEnglish(event.target.checked);
+    };
+
     return (
         <main>
-            <div className="text-neutral-50">
+            <div className="text-neutral-50 font-Montserrat">
                 {isLoading ? (
                     <Loading />
                 ) : (
-                    <ul className="font-Montserrat text-sm md:text-base">
-                        {anime.map((chars, index) => (
-                            <li key={index} className="my-4 -mx-3 md:mx-24">
-                                <div className="flex items-center justify-between">
-                                    <div className="flex items-center flex-1">
-                                        <img className="w-16 md:w-20 rounded-md" src={chars.character.images.jpg.image_url} alt={chars.character.name} />
-                                        <div className="ml-4 flex flex-col">
-                                            <p className="font-bold">{chars.character.name}</p>
-                                            <p>{chars.role}</p>
-                                        </div>
-                                    </div>
-                                    <aside className="flex flex-col items-end ml-8">
-                                        {chars.voice_actors.filter(actor => actor.language === "Japanese").map((actor, index) => (
-                                            <div key={index} className="flex items-center mb-2">
-                                                <div className="flex flex-col mr-2 text-right">
-                                                    <p>{actor.person.name}</p>
-                                                    <p>{actor.language}</p>
-                                                </div>
-                                                <img className="w-16 md:w-20 rounded-md" src={actor.person.images.jpg.image_url} alt={actor.person.name} />
+                    <>
+                        <label className="inline-flex items-center cursor-pointer md:mb-4">
+                            <input
+                                type="checkbox"
+                                className="sr-only peer"
+                                checked={showEnglish}
+                                onChange={handleToggleChange}
+                            />
+                            <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700
+                                            peer-checked:after:translate-x-full 
+                                            rtl:peer-checked:after:-translate-x-full peer-checked:after:border-gray-300 after:content-[''] after:absolute after:top-[2px] after:start-[2px]
+                                            after:bg-gray-300 after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-500"></div>
+                            <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                {showEnglish ? "EN" : "JP"}
+                            </span>
+                        </label>
+                        <ul className="font-Montserrat text-sm md:text-base">
+                            {anime.map((chars, index) => (
+                                <li key={index} className="my-4 -mx-3 md:mx-24">
+                                    <div className="flex items-center justify-between">
+                                        <div className="flex items-center flex-1">
+                                            <img className="w-16 md:w-20 rounded-md" src={chars.character.images.jpg.image_url} alt={chars.character.name} />
+                                            <div className="ml-4 flex flex-col">
+                                                <p className="font-bold">{chars.character.name}</p>
+                                                <p>{chars.role}</p>
                                             </div>
-                                        ))}
-                                    </aside>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+                                        </div>
+                                        <aside className="flex flex-col items-end ml-8">
+                                            {chars.voice_actors.filter(actor => actor.language === ( showEnglish ? "English" : "Japanese" ) ).map((actor, index) => (
+                                                <div key={index} className="flex items-center mb-2">
+                                                    <div className="flex flex-col mr-2 text-right">
+                                                        <p>{actor.person.name}</p>
+                                                        <p>{actor.language}</p>
+                                                    </div>
+                                                    <img className="w-16 md:w-20 rounded-md" src={actor.person.images.jpg.image_url} alt={actor.person.name} />
+                                                </div>
+                                            ))}
+                                        </aside>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </>
                 )}
             </div>
         </main>
